a481624fa18538e67d005f1278b1dfa799df9b78
eldonCole <cole.ralph21@gmail.com>
2024-10-15 19:14:26 -0700

    FIXED Create RobustPerceptron
    Uploaded wrong file.
    
    W.

--- RobustPerceptron.py
+++ RobustPerceptron.py
@@ -30,7 +30,7 @@ class Perceptron:
             print("Prediction: -1 (dot product < 0)\n")         # else, return -1
             return -1
         
-    def train(self, input_vectors, targets, epochs=2):
+    def train(self, input_vectors, targets, epochs=30):
         for epoch in range(epochs):                                             # repeat for as many epochs as we choose
             print(f"\nEpoch {epoch + 1}")                                       # display epoch iteration to user
             for x, t in zip(input_vectors,targets):                             # Loop through the input and targets as tuples, so the model differentiates them
@@ -75,6 +75,8 @@ class Perceptron:
     In the following, we procedurally demonstrate the capability of this Perceptron class.
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 """
+
+"""
 p = Perceptron(input_size=9, learning_rate=1)   # We begin by initializing the Perceptron model with 9 inputs (for a 3x3) and a learning rate of 1
 matrix_x1 = [                                   #matrix_x1 represents L shape
     [1, 0, 0],
@@ -102,8 +104,12 @@ x2 = [item for row in matrix_x2 for item in row]
 input_vectors = [x1, x2]                                # initialize vector inputs (list of lists)
 print("\nFlattened Vector form:\nx1", x1, "\nx2", x2)   # Display the flattened vectors for clarity
 
-p.train(input_vectors, test_targets)                    # Train our perceptron model on the input vectors and targets :)
-p.evaluate(input_vectors, test_targets)                 #evaluate whether the model is properly distinguishing the two
+#p.train(input_vectors, test_targets)                    # Train our perceptron model on the input vectors and targets :)
+#p.evaluate(input_vectors, test_targets)                 #evaluate whether the model is properly distinguishing the two
+"""
+  
+#5x5 Model implementation----------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 L_positions = []            # Now that our model is trained with 100% accuracy, lets test what it does with a 5x5 matrix.
 L_shape = np.array([        # We will organize the same L shape from the 3x3 matrix in every possible location within the 5x5
@@ -120,14 +126,40 @@ for row in range(3):                                # We loop through all valid 
         L_positions.append(flattened_grid)          #append this grid to the list of possible L placements
 
 L_targets = [-1] * len(L_positions)                 # We have now generated ever possilbe L shape position within a 5x5 matrix.
-p2 = Perceptron(input_size=25, learning_rate=1)     # Let's see how our model behaves when we train it with these inputs.
-p2.train(L_positions, L_targets)                    # This does also mean that we will have to alter our input size by initizlizing another perceptron
-p2.evaluate(L_positions, L_targets)                 # Additoinally, we will have to extend the number of targets to match the number of L shape placements
+#p2 = Perceptron(input_size=25, learning_rate=1)    # Let's see how our model behaves when we train it with these inputs.
+#p2.train(L_positions, L_targets)                   # This does also mean that we will have to alter our input size by initizlizing another perceptron
+#p2.evaluate(L_positions, L_targets)                # Additoinally, we will have to extend the number of targets to match the number of L shape placements
                                                     # all targets should be -1 because it's all 'L'
+                                                    
+                                                    
+I_positions = []            # Now that our 3x3 model is trained with 100% accuracy, lets test what it does with a 5x5 matrix.
+I_shape = np.array([        # We will organize the same L shape from the 3x3 matrix in every possible location within the 5x5
+    [1, 1, 1],              # There are 9 Possible ways to organize our L shape
+    [0, 1, 0],
+    [1, 1, 1]
+])                                                    
+for row in range(3):                                # We loop through all valid starting positions (row,col) in teh 5x5 grid. We can place the I in rows 0, 1, or 2 
+   for col in range(3):                             # Then, we can place the I in columns o 1, or 2
+        grid = np.zeros((5,5), dtype=int)           # Now, create a new 5x5 matrix of zeros
+        grid[row:row+3, col:col+3] = I_shape        # Given that we are looping within range 3, "row:row+3" means rows 0, 1, or 2 ;)
+        flattened_grid = grid.flatten().tolist()    # flatten our matrix to a vector (as list)
+        I_positions.append(flattened_grid)          # append this grid to the list of possible I placements
 
+I_targets = [1] * len(I_positions)                  # We have now generated ever possilbe I shape position within a 5x5 matrix.
+#p2.train(I_positions, I_targets)                   # This does also mean that we will have to alter our input size by initizlizing another perceptron
+#p2.evaluate(I_positions, I_targets)
+
+
+
+
+LI_Targets = [-1] * len(L_positions) + [1] * len(I_positions)   # create array of targets for each possible position (necessary)
+LI_Positions = L_positions + I_positions                        # create a list of all possible positions (list of vectors)
+
+p3 = Perceptron(25,1)                                           # instantiate new perceptron with size 25 (5x5)
+p3.train(LI_Positions,LI_Targets)                               # pass training parameters
+p3.evaluate(LI_Positions,LI_Targets)                            # validate the model after it has been trained
             
             
-
 
 
 
